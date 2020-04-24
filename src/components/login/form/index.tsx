63dd52045f66af5835cc67ad97afd0990cd4350c
joseph <josephdepompeis@gmail.com>
2020-04-23 21:05:25 -0400

    contact us post

--- src/components/login/form/index.tsx
+++ src/components/login/form/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import {IFieldProps} from "../field";
+import axios from "axios";
+import useAxios from "axios-hooks";
 
 export interface IFields {
 	[key: string]: IFieldProps;
@@ -176,17 +178,59 @@ export class Form extends React.Component<IFormProps, IFormState> {
 	 * @returns {boolean} - Whether the form is valid or not
 	 */
 	private validateForm(): boolean {
-		// TODO - validate form
-		return true;
+		const errors: IErrors = {};
+		Object.keys(this.props.fields).map((fieldName: string) => {
+			errors[fieldName] = this.validate(fieldName);
+		});
+		this.setState({ errors });
+		return !this.haveErrors(errors);
 	}
+
+
+
+
 
 	/**
 	 * Submits the form to the http api
 	 * @returns {boolean} - Whether the form submission was successful or not
 	 */
 	private async submitForm(): Promise<boolean> {
-		// TODO - submit the form
-		return true;
+
+
+		try {
+			console.log(this.state.values);
+			const response = axios.post('http://localhost:5000/contactUs', this.state.values);
+
+			// Success 🎉
+			console.log(response);
+			return true;
+		} catch (error) {
+			// Error 😨
+			if (error.response) {
+				/*
+				 * The request was made and the server responded with a
+				 * status code that falls out of the range of 2xx
+				 */
+				console.log(error.response.data);
+				console.log(error.response.status);
+				console.log(error.response.headers);
+				return error;
+
+			} else if (error.request) {
+				/*
+				 * The request was made but no response was received, `error.request`
+				 * is an instance of XMLHttpRequest in the browser and an instance
+				 * of http.ClientRequest in Node.js
+				 */
+				console.log(error.request);
+				return error;
+
+			} else {
+				// Something happened in setting up the request and triggered an Error
+				console.log('Error', error.message);
+				return error;
+			}
+		}
 	}
 
 	public render() {
